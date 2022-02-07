7b7237db97da6006e3daf1b47062e48c60c6a29d
Ashraful <ashrafulislam2880@gmail.com>
2022-02-07 15:27:26 +0600

    About Page Error fixed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,11 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-<<<<<<< HEAD
-            About Section
-=======
             <h1 className="text-4xl">A Few Words About Us</h1>
->>>>>>> Ashraful
         </div>
     );
 };
