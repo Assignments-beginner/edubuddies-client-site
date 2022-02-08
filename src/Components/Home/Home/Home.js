4ef4ad0c92f905a6c0c29555d2b66d5bbf3d6fc6
sangita <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2022-02-08 20:38:49 +0600

    teachers-added

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Teachers from '../Teachers/Teachers';
 
 const Home = () => {
     return (
         <div>
             <h1 className="text-4xl">All Home Component Call From Here</h1>
             <Banner/>
+
+            <Teachers/>
         </div>
     );
 };
