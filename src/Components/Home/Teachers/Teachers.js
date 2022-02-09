1e6edf16d43081dd2a39682687263c94d77bccd9
avishekataur <avishekataur@gmail.com>
2022-02-09 14:03:25 +0600

    teachers_setting

--- src/Components/Home/Teachers/Teachers.js
+++ src/Components/Home/Teachers/Teachers.js
@@ -1,76 +1,103 @@
-import React from 'react';
-import img1 from '../../../images/Teachers/team1.jpg';
-import img2 from '../../../images/Teachers/team2.jpg';
-import img3 from '../../../images/Teachers/team3.jpg';
-import './Teachers.css';
-
+import React from "react";
+import img1 from "../../../Images/Teachers/team1.jpg";
+import img2 from "../../../Images/Teachers/team2.jpg";
+import img3 from "../../../Images/Teachers/team3.jpg";
+import "./Teachers.css";
 
 const Teachers = () => {
   return (
-   <div className="teachers">
-     <div className="team-area">
-      <div className="section-title">
-        <h1>Our Most
-Popular Teachers</h1>
-<p>You don't have to struggle alone, you've got our assistance and help.</p>
-      </div>
-    
-      <div className="team-box">
-        <div className="box">
-          <img src={img2} alt="" />
-          <h2>Kelly Franklin</h2>
-          <span>Software Engineer</span>
-          <ul>
-            <li>
-              <a href="#"> <i class="fab fa-facebook"></i></a>
-            </li>
-            <li>
-              <a href="#"> <i className="fab fa-twitter"></i> </a>
-            </li>
-            <li>
-              <a href="#"> <i className="fab fa-linkedin"></i> </a>
-            </li>
-          </ul>
+    <div className="teachers">
+      <div className="team-area">
+        <div className="section-title">
+          <h1>Our Most Popular Teachers</h1>
+          <p>
+            You don't have to struggle alone, you've got our assistance and
+            help.
+          </p>
         </div>
 
-{/* ----------------------------- */}
-<div className="box">
-          <img src={img1} alt="" />
-          <h2>Rakib Siddique</h2>
-          <span>Software Engineer</span>
-          <ul>
-            <li>
-              <a href="#"> <i class="fab fa-facebook"></i></a>
-            </li>
-            <li>
-              <a href="#"> <i className="fab fa-twitter"></i> </a>
-            </li>
-            <li>
-              <a href="#"> <i className="fab fa-linkedin"></i> </a>
-            </li>
-          </ul>
-        </div>
-        {/* ------------------------ */}
-        <div className="box">
-          <img src={img3} alt="" />
-          <h2>J.K Alice</h2>
-          <span>Software Engineer</span>
-          <ul>
-            <li>
-              <a href="#"> <i class="fab fa-facebook"></i></a>
-            </li>
-            <li>
-              <a href="#"> <i className="fab fa-twitter"></i> </a>
-            </li>
-            <li>
-              <a href="#"> <i className="fab fa-linkedin"></i> </a>
-            </li>
-          </ul>
-        </div>
+        <div className="team-box">
+          <div className="box">
+            <img src={img2} alt="" />
+            <h2>Kelly Franklin</h2>
+            <span>Software Engineer</span>
+            <ul>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i class="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i className="fab fa-twitter"></i>{" "}
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i className="fab fa-linkedin"></i>{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
 
+          {/* ----------------------------- */}
+          <div className="box">
+            <img src={img1} alt="" />
+            <h2>Rakib Siddique</h2>
+            <span>Software Engineer</span>
+            <ul>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i class="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i className="fab fa-twitter"></i>{" "}
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i className="fab fa-linkedin"></i>{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* ------------------------ */}
+          <div className="box">
+            <img src={img3} alt="" />
+            <h2>J.K Alice</h2>
+            <span>Software Engineer</span>
+            <ul>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i class="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i className="fab fa-twitter"></i>{" "}
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <i className="fab fa-linkedin"></i>{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-   </div>
   );
 };
 
