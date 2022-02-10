f5326e4051493b24e69ac40df0cd6ac2c5c489e5
avishekataur <avishekataur@gmail.com>
2022-02-10 13:20:27 +0600

    profile_img_done

--- src/Components/UserProfile/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile/UserProfile.js
@@ -1,22 +1,21 @@
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
+import "../UserProfile/UserProfile.css";
 
 const UserProfile = () => {
   const { user } = useAuth();
   return (
     <div className="mt-16 mb-36">
       {user?.email && (
-        <div className="flex flex-col items-center justify-items-center profile-imgName">
+        <div className="flex flex-col items-center justify-items-center">
           <img
-            className="user-img rounded-full ring-2 ring-offset-2"
+            className="userProfile-img rounded-full ring-2 ring-offset-2"
             height="100"
             width="100"
             src={user.photoURL}
             alt=""
           />
-          <span
-            className="text-black text-4xl ml-2 mt-8 uppercase"
-          >
+          <span className="text-black text-4xl ml-2 mt-8 uppercase">
             {user.displayName}
           </span>
         </div>
