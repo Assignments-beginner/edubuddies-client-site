cd697607a95590f462499b5d6c8b0b7ea7748a2a
avishekataur <avishekataur@gmail.com>
2022-02-10 13:27:12 +0600

    firebase_merge

--- src/Components/UserProfile/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile/UserProfile.js
@@ -8,7 +8,7 @@ const UserProfile = () => {
     <div className="mt-16 mb-36">
       {user?.email && (
         <div className="flex flex-col items-center justify-items-center">
-          <img
+          <img 
             className="userProfile-img rounded-full ring-2 ring-offset-2"
             height="100"
             width="100"
