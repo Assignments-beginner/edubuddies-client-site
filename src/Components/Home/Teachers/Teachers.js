1fa8c02b49239963343ed7d1af6a850c409cf862
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2022-02-10 20:47:00 +0600

    firebase key and some design

--- src/Components/Home/Teachers/Teachers.js
+++ src/Components/Home/Teachers/Teachers.js
@@ -5,113 +5,113 @@ import img3 from "../../../Images/Teachers/team3.jpg";
 import "./Teachers.css";
 
 const Teachers = () => {
-  return (
-    <div className="teachers">
-      <div className="team-area">
-        <div className="text-5xl font-black mt-5 mb-8 text-blue-700">
-          Our Beloved Teachers
-          <br />
-          <span className="text-sm text-black">
-            You don't have to struggle alone, you've got our assistance and
-            help.
-          </span>
-        </div>
+	return (
+		<div className='teachers'>
+			<div className='team-area'>
+				<div className='text-5xl font-black mt-5 mb-8 text-blue-700'>
+					Our Beloved Teachers
+					<br />
+					<span className='text-sm text-black'>
+						You don't have to struggle alone, you've got our assistance and
+						help.
+					</span>
+				</div>
 
-        <div className="team-box">
-          <div className="box">
-            <div className="teachers-box">
-              <img src={img2} alt="" />
-            </div>
-            <div>
-              <h2 className="mt-4">Kelly Franklin</h2>
-              <span>Senior Developer</span>
-              <ul>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i class="fab fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i className="fab fa-twitter"></i>{" "}
-                  </a>
-                </li>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i className="fab fa-linkedin"></i>{" "}
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+				<div className='team-box'>
+					<div className='box'>
+						<div className='teachers-box'>
+							<img src={img2} alt='' />
+						</div>
+						<div>
+							<h2 className='mt-4'>Kelly Franklin</h2>
+							<span>Senior Developer</span>
+							<ul>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-facebook'></i>
+									</a>
+								</li>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-twitter'></i>{" "}
+									</a>
+								</li>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-linkedin'></i>{" "}
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
 
-          {/* ----------------------------- */}
-          <div className="box">
-            <div className="teachers-box">
-              <img src={img1} alt="" />
-            </div>
-            <div>
-              <h2 className="mt-4">Rakib Siddique</h2>
-              <span>Software Engineer</span>
-              <ul>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i class="fab fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i className="fab fa-twitter"></i>{" "}
-                  </a>
-                </li>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i className="fab fa-linkedin"></i>{" "}
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* ------------------------ */}
-          <div className="box">
-            <div className="teachers-box">
-              <img src={img3} alt="" />
-            </div>
-            <div>
-              <h2 className="mt-4">J.K Alice</h2>
-              <span>Web Developer</span>
-              <ul>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i class="fab fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i className="fab fa-twitter"></i>{" "}
-                  </a>
-                </li>
-                <li>
-                  <a href="/#">
-                    {" "}
-                    <i className="fab fa-linkedin"></i>{" "}
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					{/* ----------------------------- */}
+					<div className='box'>
+						<div className='teachers-box'>
+							<img src={img1} alt='' />
+						</div>
+						<div>
+							<h2 className='mt-4'>Rakib Siddique</h2>
+							<span>Software Engineer</span>
+							<ul>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-facebook'></i>
+									</a>
+								</li>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-twitter'></i>{" "}
+									</a>
+								</li>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-linkedin'></i>{" "}
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					{/* ------------------------ */}
+					<div className='box'>
+						<div className='teachers-box'>
+							<img src={img3} alt='' />
+						</div>
+						<div>
+							<h2 className='mt-4'>J.K Alice</h2>
+							<span>Web Developer</span>
+							<ul>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-facebook'></i>
+									</a>
+								</li>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-twitter'></i>{" "}
+									</a>
+								</li>
+								<li>
+									<a href='/#'>
+										{" "}
+										<i className='fab fa-linkedin'></i>{" "}
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Teachers;
--- src/Components/Login/Firebase/firebase.config.js
+++ src/Components/Login/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+	apiKey: "AIzaSyDM6DY-LzcOWClXt5v2IKUNyAtLzGmudjs",
+	authDomain: "edu-buddies.firebaseapp.com",
+	projectId: "edu-buddies",
+	storageBucket: "edu-buddies.appspot.com",
+	messagingSenderId: "929610407135",
+	appId: "1:929610407135:web:97c284a6eef75ff042dd6a",
 };
 export default firebaseConfig;
