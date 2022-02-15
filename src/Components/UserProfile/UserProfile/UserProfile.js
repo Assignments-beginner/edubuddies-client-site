bb404c4473595927da8b2e7c3999c7fdae215791
unknown <nizamcse.seu@gmail.com>
2022-02-15 07:04:01 +0600

    changes

--- src/Components/UserProfile/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile/UserProfile.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const UserProfile = () => {
-    return (
-        <div>
-            <h1 className="text-4xl">User Profile Details Wiil Goes here</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-4xl">User Profile Details Wiil Goes here</h1>
+    </div>
+  );
 };
 
 export default UserProfile;
