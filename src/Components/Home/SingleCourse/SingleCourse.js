26a76c90ed8fa8374417b94b494554c5fa72505a
avishekataur <avishekataur@gmail.com>
2022-02-28 07:31:50 +0600

    flex_col_reverse_responsive

--- src/Components/Home/SingleCourse/SingleCourse.js
+++ src/Components/Home/SingleCourse/SingleCourse.js
@@ -19,9 +19,9 @@ const SingleCourse = () => {
   console.log(sigleData);
 
   return (
-    <div className="grid lg:grid-cols-2 sm:grid-cols-1 container mx-auto pb-24 px-6">
-      {/* Course Details  */}
-      <div>
+    <div className="entireCourse pb-24 container mx-auto">
+      {/* Course Details /////////////////////////////////////////////  */}
+      <div className="instructor px-4">
         <h1 className="text-3xl text-left font-bold mt-8">
           {sigleData?.title}
         </h1>
@@ -51,44 +51,46 @@ const SingleCourse = () => {
           </div>
         </div>
       </div>
-      {/* Course Card Details  */}
-      <div className="rounded overflow-hidden shadow-lg mt-12">
-        <img className="w-full" src={sigleData.image} alt="" />
-        <div className="border-2 h-12 w-72 mt-4 mx-auto"></div>
-        {/* Hours + Enrolled  */}
-        <div className="flex justify-between pt-8 px-10">
-          <div className="flex items-center">
-            <FontAwesomeIcon
-              className="text-slate-900 pr-2 text-2xl"
-              icon={faUsers}
-            />
-            <span>Enrolled 1232</span>
+      {/* Course Card Details  //////////////////////////////////////// */}
+      <div className="px-4 courseCard">
+        <div className="rounded overflow-hidden shadow-lg">
+          <img className="w-full" src={sigleData.image} alt="" />
+          <div className="border-2 h-12 w-72 mt-4 mx-auto"></div>
+          {/* Hours + Enrolled  */}
+          <div className="flex justify-between pt-8 px-10">
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                className="text-slate-900 pr-2 text-2xl"
+                icon={faUsers}
+              />
+              <span>Enrolled 1232</span>
+            </div>
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                className="text-slate-900 pr-2 text-2xl"
+                icon={faClockFour}
+              />
+              <span>{sigleData?.courseDuration} Hours</span>
+            </div>
           </div>
-          <div className="flex items-center">
-            <FontAwesomeIcon
-              className="text-slate-900 pr-2 text-2xl"
-              icon={faClockFour}
-            />
-            <span>{sigleData?.courseDuration} Hours</span>
+          <hr className="w-5/6 mt-6 mx-auto" />
+          {/* Enroll Button  */}
+          <div className="px-4 pt-8 pb-3">
+            <div className="flex justify-between items-center px-4 pb-2">
+              <span>Promo Code</span>
+              <span className="text-3xl">$ {sigleData?.courseFee}</span>
+            </div>
+            <button
+              className="bg-red-700 hover:bg-red-800 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full flex items-center justify-center"
+              type="submit"
+            >
+              Get This Course &nbsp;&nbsp;
+              <FontAwesomeIcon
+                className="text-white pr-2 text-xl"
+                icon={faArrowRight}
+              />
+            </button>
           </div>
-        </div>
-        <hr className="w-5/6 mt-6 mx-auto" />
-        {/* Enroll Button  */}
-        <div className="px-4 pt-8 pb-3">
-          <div className="flex justify-between items-center px-4 pb-2">
-            <span>Promo Code</span>
-            <span className="text-3xl">$ {sigleData?.courseFee}</span>
-          </div>
-          <button
-            className="bg-red-700 hover:bg-red-800 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full flex items-center justify-center"
-            type="submit"
-          >
-            Get This Course &nbsp;&nbsp;
-            <FontAwesomeIcon
-              className="text-white pr-2 text-xl"
-              icon={faArrowRight}
-            />
-          </button>
         </div>
       </div>
     </div>
