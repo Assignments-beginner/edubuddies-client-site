ed0d4ce06758d91e9f410f3440450182b92cf83f
Ashraful <ashrafulislam2880@gmail.com>
2022-03-01 14:54:30 +0600

    Dashboard Route Added

---
+++ src/Components/TeachersDashboard/AddCourse/AddCourse.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddCourse = () => {
+  return (
+    <div>
+      <h1 className="text-5xl">Add A Course</h1>
+    </div>
+  );
+};
+
+export default AddCourse;
---
+++ src/Components/TeachersDashboard/RemoveCourse/RemoveCourse.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RemoveCourse = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Remove a Course</h1>
+    </div>
+  );
+};
+
+export default RemoveCourse;
