798d032b0ae2f08c16c190c38b357ad4f81c3cb9
Ashraful <ashrafulislam2880@gmail.com>
2022-03-01 20:25:39 +0600

    All Teachers API Created

--- src/Components/TeachersDashboard/AllTeachers/AllTeachers.js
+++ src/Components/TeachersDashboard/AllTeachers/AllTeachers.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AllTeachers = () => {
+  const [teachers, setTeachers] = useState([]);
+  useEffect(() => {
+    fetch("https://fierce-caverns-90976.herokuapp.com/teachers")
+      .then((res) =>
+        res.json("https://fierce-caverns-90976.herokuapp.com/teachers")
+      )
+      .then((data) => setTeachers(data));
+  }, []);
   return (
     <div>
       <h1 className="text-3xl">All Teachers</h1>
