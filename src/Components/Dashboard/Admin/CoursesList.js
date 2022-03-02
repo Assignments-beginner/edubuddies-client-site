019467122cbde3ca2cdd3829d61ccec19544e568
unknown <nizamcse.seu@gmail.com>
2022-03-02 22:57:50 +0600

    recyclebin is donee

--- src/Components/Dashboard/Admin/CoursesList.js
+++ src/Components/Dashboard/Admin/CoursesList.js
@@ -14,33 +14,6 @@ const CoursesList = () => {
     });
   }, [courses]);
 
-  const deleteCOurse = (id) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios
-          .delete(`http://localhost:5000/deleteCourses/${id}`)
-          .then((res) => {
-            if (res.data.deletedCount > 0) {
-              Swal.fire({
-                showConfirmButton: false,
-                icon: "success",
-                title: "Your file has been deleted",
-                timer: 1000,
-              });
-            }
-          });
-      }
-    });
-  };
-
   // courses update
   const updateCourseStatus = (id, statusName) => {
     const status = {
