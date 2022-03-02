2be69c35cdbe47b142fce2ee467417126199cbd9
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-02 23:31:32 +0600

    Single Teacher Page Design Done

--- src/Components/TeachersDashboard/TeachersProfile/TeachersProfile.js
+++ src/Components/TeachersDashboard/TeachersProfile/TeachersProfile.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import CountUp from "react-countup";
 import { useParams } from "react-router-dom";
 
 const TeachersProfile = () => {
@@ -12,27 +13,40 @@ const TeachersProfile = () => {
   }, []);
 
   return (
-    <div className=" container mx-auto mt-4">
+    <div className=" container mt-4 px-10">
       <div className="flex flex-row">
-        <div className="basis-1/4 text-left border">
-          <img src={singleTeacher?.image} alt="Teachers" />
-          <div className="flex justify-between px-5 py-2 bg-gray-200">
-            <h4 className="text-xl">{singleTeacher?.name}</h4>
-            <h4>{singleTeacher?.designation}</h4>
+        <div className="basis-1/4 text-left border rounded-md">
+          <img
+            className="rounded-t-md"
+            src={singleTeacher?.image}
+            alt="Teachers"
+          />
+          <div className="flex justify-between items-center border-b-2 border-red-500 px-5 py-2 bg-gray-800 font-bold tracking-widest">
+            <h4 className="text-lg text-red-500">{singleTeacher?.name}</h4>
+            <h4 className="text-white">{singleTeacher?.designation}</h4>
           </div>
-          <div className="pl-5 shadow-md">
-            <h4>Email: {singleTeacher?.email}</h4>
-            <h4>Phone: {singleTeacher?.phone}</h4>
-            <h4>Age: {singleTeacher?.age}</h4>
-            <h4>Gender: {singleTeacher?.gender}</h4>
+          <div className="pl-5 shadow-md text-lg pt-2 pb-7">
             <h4>
-              Address: {singleTeacher?.address?.street},
-              {singleTeacher?.address?.city},{singleTeacher?.address?.zipcode}
+              <span className="font-bold">Email:</span> {singleTeacher?.email}
+            </h4>
+            <h4>
+              <span className="font-bold">Phone:</span> {singleTeacher?.phone}
+            </h4>
+            <h4>
+              <span className="font-bold">Age:</span> {singleTeacher?.age}
+            </h4>
+            <h4>
+              <span className="font-bold">Gender:</span> {singleTeacher?.gender}
+            </h4>
+            <h4>
+              <span className="font-bold">Address:</span>{" "}
+              {singleTeacher?.address?.street}, {singleTeacher?.address?.city},{" "}
+              {singleTeacher?.address?.zipcode}
             </h4>
             <h4>{singleTeacher?.country}</h4>
           </div>
         </div>
-        <div className="basis-3/4 ml-10 border shadow-md">
+        <div className="basis-3/4 ml-10 border rounded-md shadow-md pb-5">
           <h4 className="text-left text-2xl p-5 font-semibold">
             About Of
             <span className="text-red-500 text-3xl">
@@ -41,38 +55,54 @@ const TeachersProfile = () => {
             </span>
           </h4>
           <hr />
-          <div className="pl-5 pt-2 text-left">
-            <p className="text-gray-500 mb-6">{singleTeacher?.about}</p>
-            <h2 className="text-2xl">Skills</h2>
+          <div className="pl-5 text-left">
+            <p className="text-gray-500 my-4 tracking-wider">
+              {singleTeacher?.about}
+            </p>
+            <h2 className="text-2xl font-bold">Skills</h2>
             <h4 className="my-6">
               {singleTeacher?.Experties?.map((expert, key) => (
                 <span
-                  className="mr-4 bg-gray-300 py-2 px-4 rounded-md"
+                  className="mr-4 bg-gray-800 text-red-500 py-2 px-4 rounded-md"
                   key={key}
                 >
                   {expert}
                 </span>
               ))}
             </h4>
-            <h2 className="text-2xl">Language</h2>
+            <h2 className="text-2xl font-bold">Language</h2>
             <h4 className="my-6">
               {singleTeacher?.language?.map((lang, key) => (
                 <span
-                  className="mr-4 bg-gray-300 py-2 px-4 rounded-md"
+                  className="mr-4 bg-gray-800 py-2 px-4 text-red-500 rounded-md"
                   key={key}
                 >
                   {lang}
                 </span>
               ))}
             </h4>
-            <h2 className="text-xl mb-">
-              Experience: {singleTeacher?.experinece} Years
+            <h2 className="text-lg mb-2">
+              <span className="font-bold">Experience: </span>
+              {singleTeacher?.experinece} Years
             </h2>
-            <h2 className="text-xl">
-              Operation Done: {singleTeacher?.operationDone}
+            <h2 className="text-lg mb-2">
+              <span className="font-bold">Operation Done: </span>
+              <CountUp
+                start={0}
+                end={singleTeacher?.operationDone}
+                duration={4.25}
+              />
             </h2>
-            <h2 className="text-xl">Join Date: 10-February-2020</h2>
-            <h2 className="text-xl mb-4">Job Type: {singleTeacher?.type}</h2>
+            <h2 className="text-lg mb-2">
+              <span className="font-bold">Join Date: </span> 10-February-2020
+            </h2>
+            <h2 className="text-lg mb-4">
+              <span className="font-bold">Job Type: </span>
+              {singleTeacher?.type}
+            </h2>
+            <button className="bg-red-500 border border-red-500 text-white px-4 py-2 rounded-md hover:bg-transparent hover:text-red-500">
+              View Published Post
+            </button>
           </div>
         </div>
       </div>
