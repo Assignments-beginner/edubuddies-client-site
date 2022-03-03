2d9b9cbcf39986efd8012cd867b2436614202985
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-03 18:05:19 +0600

    My Course Page Design Added

--- src/Components/TeachersDashboard/MyCourseDeatails/MyCourseDetails.js
+++ src/Components/TeachersDashboard/MyCourseDeatails/MyCourseDetails.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 const MyCourseDetails = () => {
   const { courseId } = useParams();
-  const [courseDetails, setcourseDetails] = useState([]);
+  const [courseDetails, setcourseDetails] = useState({});
   useEffect(() => {
     fetch(`http://localhost:5000/CourseDetails/${courseId}`)
       .then((res) => res.json())
@@ -12,7 +12,41 @@ const MyCourseDetails = () => {
   console.log(courseDetails);
   return (
     <div>
-      <h1 className="text-3xl text-red-600">Details Of Your Course</h1>
+      <h1 className="text-3xl text-red-600 mt-4 mb-10 font-bold">
+        Details Of Your Course
+      </h1>
+      <div className="lg:w-2/5 md:w-full sm:w-full mx-auto border rounded-md shadow-xl">
+        <img
+          className="mx-auto rounded-md"
+          src={courseDetails?.image}
+          alt="Course Bannar"
+        />
+        <div className="text-left px-5 pb-10 border-t-2 border-red-500">
+          <h2 className="text-2xl mt-4 font-bold">
+            {courseDetails?.title} By{" "}
+            <span className="text-red-500">{courseDetails?.owner?.name}</span>
+          </h2>
+          <h2 className="text-xl mt-4">
+            <span className="font-bold">Email: </span>
+            {courseDetails?.owner?.email}
+          </h2>
+          <h4 className="text-xl mt-4">
+            <span className="font-bold">Category: </span>
+            {courseDetails?.category}
+          </h4>
+          <h4 className="text-xl mt-4">
+            <span className="font-bold">Duration: </span>
+            {courseDetails?.courseDuration} Month
+          </h4>
+          <h4 className="text-xl mt-4">
+            <span className="font-bold">Course Fee: </span>
+            {courseDetails?.courseFee}
+          </h4>
+          <button className="border border-red-500 py-2 px-6 rounded-md mt-4 bg-red-500 text-white hover:text-red-500 hover:bg-transparent duration-300">
+            Edit Course
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
