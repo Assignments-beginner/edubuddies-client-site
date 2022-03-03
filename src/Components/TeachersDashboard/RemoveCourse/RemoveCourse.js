b28911dce462093b5af7709a7dc602ba21742d96
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-03 22:52:54 +0600

    Quize Section Added

--- src/Components/TeachersDashboard/RemoveCourse/RemoveCourse.js
+++ src/Components/TeachersDashboard/RemoveCourse/RemoveCourse.js
@@ -1,9 +1,47 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addCourse } from "../../../Redux/edubuddySlice";
 
 const RemoveCourse = () => {
+  const dispatch = useDispatch();
+  const courses = useSelector((state) => state.edu.courses);
+
+  useEffect(() => {
+    const url = "https://fierce-caverns-90976.herokuapp.com/courses";
+    axios.get(url).then((res) => {
+      dispatch(addCourse(res.data));
+    });
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1 className="text-3xl text-red-600 font-bold mt-3">Remove a Course</h1>
+    <div className="container mx-auto mt-4 px-4 md:px-11  ">
+      <h1 className="text-center text-3xl uppercase font-semibold md:mb-9 mb-5 text-red-500">
+        Romove Your Course From List
+      </h1>
+      <div className="mx-auto grid grid-cols-12 gap-9">
+        {courses &&
+          courses?.map((item, key) => (
+            <div className="md:col-span-4 col-span-12" key={key}>
+              <Link to={`/teachersDashboard/CourseDetails/${item?._id}`}>
+                <div
+                  className="border rounded-lg card mx-auto duration-300 bg-white"
+                  style={{ maxWidth: "400px" }}
+                >
+                  <div className="overflow-hidden">
+                    <img
+                      className="object-fill h-52 w-full card-image rounded-t-lg"
+                      src={item?.image}
+                      alt={item?.title}
+                    />
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
