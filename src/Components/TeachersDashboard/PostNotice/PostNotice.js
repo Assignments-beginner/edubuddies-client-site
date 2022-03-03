d549d6ec42b1b06685a46881cd9f160bca0e8ffe
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-03 15:40:41 +0600

    Add Courses Page Added

--- src/Components/TeachersDashboard/PostNotice/PostNotice.js
+++ src/Components/TeachersDashboard/PostNotice/PostNotice.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const PostNotice = () => {
+  const { handleSubmit } = useForm();
+
+  const onSubmit = () => {};
+
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  const [convertedContent, setConvertedContent] = useState(null);
+  console.log(convertedContent);
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  };
+
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl text-red-600">Post a Notice For Students</h1>
+    <div className="container mx-auto px-4 md:px-11">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col space-y-3"
+      >
+        <div className="relative w-full my-6 mx-auto max-w-3xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <h3 className="text-3xl font-semibold text-center text-red-500">
+              Post A Notice For Students
+            </h3>
+            <div>
+              <Editor
+                editorState={editorState}
+                onEditorStateChange={handleEditorChange}
+                wrapperClassName="wrapper"
+                editorClassName="editor"
+                toolbarClassName="toolbar"
+              />
+            </div>
+            <button
+              className="bg-red-500 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="submit"
+            >
+              Post Notice
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
