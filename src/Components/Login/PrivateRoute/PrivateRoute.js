c2f3629c0200310b9e5f6b11731d3b329ef4f8a0
avishekataur <avishekataur@gmail.com>
2022-03-09 21:09:15 +0600

    private_route_test_done

---
+++ src/Components/Login/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user, isLoading } = useAuth();
+  let location = useLocation();
+  if (isLoading) {
+    return <div className="animate-spin"></div>;
+  }
+  if (user.email) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} />;
+};
+
+export default PrivateRoute;
