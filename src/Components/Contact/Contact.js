be8f56743e884dfb8001e392a6945d94f7a527dc
avishekataur <avishekataur@gmail.com>
2022-03-11 07:37:05 +0600

    editing_contact_form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,80 +1,75 @@
-import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-import emailjs from '@emailjs/browser';
-import './Contact.css';
+import React from "react";
 
 const Contact = () => {
-
-    const [formMsg, setFormMsg] = useState(true);
-
-  
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-  
-        emailjs.sendForm(
-            'service_nv4khp1', //service ID
-            'template_dka5cee', //template id
-            event.target,
-            'user_LHVMNnpoRh5NOCx3QAi6S' //user ID
-        ).then(result => {
-            console.log('contact me result', result);
-  
-            
-  
-            if (result.status === 200) {
-                setFormMsg(false)
-                let timerInterval
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Thanks you!',
-                    text: 'Thanks! We will get back to you as soon as possible.',
-                    timer: 5000,
-                    timerProgressBar: true,
-                    didOpen: () => {
-                        Swal.showLoading()
-                    },
-                    willClose: () => {
-                        clearInterval(timerInterval)
-                    }
-                }).then((result) => {
-                    /* Read more about handling dismissals below */
-                    if (result.dismiss === Swal.DismissReason.timer) {
-                        console.log('I was closed by the timer')
-                    }
-                })
-            }
-  
-        }).catch(error => {
-            console.log('error', error);
-            // setFormMsg(error)
-        })
-        event.target.reset();
-    }
-
-    return (
-        <div className="contact py-16">
-
-
-<div className="contact-map px-7 mx-5">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.1312072706355!2d91.41332071455426!3d22.982202184973552!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd8f065795d95%3A0xf921aca48f8cf70a!2sDhaka%20-%20Chittagong%20Hwy%2C%20Bangladesh!5e0!3m2!1sen!2sde!4v1646679709300!5m2!1sen!2sde" width="600" height="auto"  loading="lazy"></iframe>
-</div>
-
-
-
-            <div className="contact-box">                
-                {/* <form className="contact-form" onsubmit="sendEmail(); reset(); return false;"> */}
-                <form className="contact-form" onSubmit={handleFormSubmit} >
-                    <h3 className="mt-4 text-2xl">GET IN TOUCH</h3>
-                    <input type="text" id="name" name="User Name" placeholder="Your Name" required />
-                    <input type="email" id="email" placeholder="Your Email" required name="email"/>
-                    <input type="text" id="phone" placeholder="Your Phone No." required />
-                    <textarea id="message" rows="4" placeholder="How can we help you?"></textarea>
-                    <button type="submit">Send</button>
-                </form>
-            </div>
+  return (
+    <div className="max-w-screen-xl mt-24 px-16 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-16 lg:px-16 xl:px-16 py-16 mx-auto text-gray-900">
+      {/* // Contact Form */}
+      <div>
+        <div>
+          <h1 className="text-left text-3xl uppercase font-semibold md:mb-9 mb-5 text-red-500">
+            Contact Us
+          </h1>
         </div>
-    );
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+          <div className="text-left">
+            {/* <span className="uppercase text-sm text-gray-600 font-bold">
+              Full Name
+            </span> */}
+            <input
+              className="w-full bg-slate-100 text-gray-900 mt-2 py-3 px-4 rounded-lg"
+              type="text"
+              placeholder="Full Name"
+            />
+          </div>
+          <div className="text-left">
+            {/* <span className="uppercase text-sm text-gray-600 font-bold">
+              Email
+            </span> */}
+            <input
+              className="w-full bg-slate-100 text-gray-900 mt-2 py-3 px-4 rounded-lg"
+              type="text"
+              placeholder="Email"
+            />
+          </div>
+        </div>
+        <div className="mt-3 text-left">
+          {/* <span className="uppercase text-sm text-gray-600 font-bold">
+              Subject
+            </span> */}
+          <input
+            className="w-full bg-slate-100 text-gray-900 mt-2 py-3 px-4 rounded-lg"
+            type="text"
+            placeholder="Subject"
+          />
+        </div>
+        <div className="mt-3 text-left">
+          {/* <span className="uppercase text-sm text-gray-600 font-bold">
+              Message
+            </span> */}
+          <textarea
+            className="w-full h-32 bg-slate-100 text-gray-900 mt-2 py-3 px-4 rounded-lg"
+            placeholder="Message"
+          ></textarea>
+        </div>
+        <div className="mt-3 text-left">
+          <button className="uppercase text-sm tracking-wide bg-gray-900 text-gray-100 py-3 px-4 hover:bg-gray-800 rounded-lg w-64">
+            Send Message
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col justify-between">
+        <div>
+          <h2 className="text-4xl lg:text-5xl font-bold leading-tight">
+            Lets talk about everything!
+          </h2>
+          <div className="text-gray-700 mt-3">
+            Hate forms? Send us an <span className="underline">email</span>{" "}
+            instead.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
