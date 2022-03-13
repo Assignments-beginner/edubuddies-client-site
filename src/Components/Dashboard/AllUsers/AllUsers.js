2008dbed0fa37d2bdf4555eb53820aa9022900bf
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2022-03-13 13:17:26 +0600

    table width height

--- src/Components/Dashboard/AllUsers/AllUsers.js
+++ src/Components/Dashboard/AllUsers/AllUsers.js
@@ -61,13 +61,7 @@ const AllUsers = () => {
 			<div className='flex flex-col'>
 				<div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
 					<div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-						<div
-							className='shadow overflow-hidden border-b border-red-200 sm:rounded-lg'
-							style={{
-								maxWidth: "90vw",
-								height: "70vh",
-								overflow: "scroll",
-							}}>
+						<div className='shadow overflow-hidden border-b border-red-200 sm:rounded-lg tables'>
 							<table className='min-w-full divide-y divide-red-200'>
 								<thead className='bg-black font-bold'>
 									<tr>
--- src/Components/Home/SupportSession/NeedSupportDetailsModal.js
+++ src/Components/Home/SupportSession/NeedSupportDetailsModal.js
@@ -75,13 +75,7 @@ const NeedSupportDetailsModal = ({ handleSolveId, setHandleSolveModal }) => {
 					<div className='flex flex-col'>
 						<div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
 							<div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-								<div
-									className='shadow overflow-hidden border-b border-red-200 sm:rounded-lg m-4'
-									style={{
-										maxWidth: "90vw",
-										height: "70vh",
-										overflow: "scroll",
-									}}>
+								<div className='shadow overflow-hidden border-b border-red-200 sm:rounded-lg m-4 tables'>
 									<table className='min-w-full divide-y divide-red-200'>
 										<thead className='bg-black font-bold'>
 											<tr>
