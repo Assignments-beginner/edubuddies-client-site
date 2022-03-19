528014b0b622379931548b375dac69e36ca125d5
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-19 15:09:30 +0600

    My Courses Page Structure done

--- src/Components/StudentDashboard/MyClasess/MyClasess.js
+++ src/Components/StudentDashboard/MyClasess/MyClasess.js
@@ -3,7 +3,7 @@ import React from "react";
 const MyClasess = () => {
   return (
     <div className="mt-5">
-      <h1 className="text-3xl text-red-500 font-bold">My Courses</h1>
+      <h1 className="text-3xl text-red-500 font-bold">My All Courses</h1>
     </div>
   );
 };
