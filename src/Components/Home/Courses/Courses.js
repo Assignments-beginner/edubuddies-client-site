15e8df33698858af6deb64cdcb899a6bc6403274
avishekataur <avishekataur@gmail.com>
2022-03-22 19:42:15 +0600

    cart_form

--- src/Components/Home/Courses/Courses.js
+++ src/Components/Home/Courses/Courses.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faStar,
+  faBookmark as solidBookmark,
+} from "@fortawesome/free-solid-svg-icons";
 import {
   faUser,
   faBookmark,
@@ -21,11 +24,13 @@ import { addCourse } from "../../../Redux/edubuddySlice";
 import demoUser from "../../../Images/user-demo.png";
 import Swal from "sweetalert2";
 import useAuth from "../../../hooks/useAuth";
+import { useForm } from "react-hook-form";
 
 SwiperCore.use([Autoplay, Pagination]);
 
 const Courses = () => {
   const { user } = useAuth();
+  const { register, handleSubmit } = useForm();
   const courses = useSelector((state) => state?.edu?.courses);
   const dispatch = useDispatch();
 
@@ -37,11 +42,23 @@ const Courses = () => {
   }, [dispatch]);
   console.log(courses);
 
-  const openAlert = () => {
+  /* const openAlert = () => {
     Swal.fire({
       title: "Course Added To Wishlist",
     });
+  }; */
+
+  // new work
+  const onSubmit = (data) => {
+    axios.post("http://localhost:5000/addCourseToCart", data).then((res) => {
+      if (res.data.insertedId) {
+        Swal.fire({
+          title: "Course Added To Wishlist",
+        });
+      }
+    });
   };
+  // new work
 
   return (
     <div className="container mb-14 mx-auto px-4 md:px-11">
@@ -78,7 +95,8 @@ const Courses = () => {
                     src={course?.image}
                     alt=""
                   />
-                  {user.email ? (
+                  {/* old button */}
+                  {/* {user.email ? (
                     <button
                       onClick={openAlert}
                       className="
@@ -110,7 +128,75 @@ const Courses = () => {
                         icon={faBookmark}
                       />
                     </Link>
-                  )}
+                  )} */}
+                  {/* old button */}
+                  {/* new test work  */}
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                      className="hidden"
+                      defaultValue={course?.image}
+                      {...register("cartCourseImage")}
+                    />
+                    <input
+                      className="hidden"
+                      defaultValue={course?.title}
+                      {...register("cartCourseTitle")}
+                    />
+                    <input
+                      className="hidden"
+                      defaultValue={course?.courseFee}
+                      {...register("cartCourseFee")}
+                    />
+                    <input
+                      className="hidden"
+                      defaultValue="Added"
+                      {...register("cartCourseStatus")}
+                    />
+                    <input
+                      className="hidden"
+                      defaultValue={course?.owner?.name}
+                      {...register("cartUserName")}
+                    />
+                    <input
+                      className="hidden"
+                      defaultValue={course?.owner?.email}
+                      {...register("cartUserEmail")}
+                    />
+                    {user.email ? (
+                      <button
+                        type="submit"
+                        className="
+                    top-4 
+                    right-4 
+                    absolute 
+                    px-1.5
+                    pt-1.5
+                    bg-white"
+                      >
+                        <FontAwesomeIcon
+                          className="text-red-500 text-xl"
+                          icon={faBookmark}
+                        />
+                      </button>
+                    ) : (
+                      <Link
+                        to="/signup"
+                        className="
+                    top-4 
+                    right-4 
+                    absolute 
+                    px-1.5
+                    pt-1.5
+                    bg-white"
+                      >
+                        <FontAwesomeIcon
+                          className="text-red-500 text-xl"
+                          icon={faBookmark}
+                        />
+                      </Link>
+                    )}
+                  </form>
+                  {/* new test work  */}
                   <div className="bg-slate-900 top-4 left-4 absolute px-2 rounded">
                     <span className="text-white text-xs">
                       {course?.category}
